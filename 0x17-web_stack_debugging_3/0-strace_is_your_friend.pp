63de979b490e49105710473a6a61813f3358ace5
Ntsanii <ntsane01m@gmail.com>
2024-08-13 14:37:56 -0700

    Strace is your friend

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fixes a specific issue in the wp-settings.php file of a WordPress installation.
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
